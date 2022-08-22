b1e651d9b15955f0f052711e0800411b4d236f24
285muhammad <285muhammad@gmail.com>
2022-08-22 04:20:36 +0200

    create remove_front function and print function for the sturct of hospital queue and finish it

--- system.cpp
+++ system.cpp
@@ -37,18 +37,57 @@ struct hospital_queue
             cout<<"There is on places Sorry"<<endl;
             return;
         }
+
         for(int i=len;i<0;i++){
             names[len]=names[len-1];
             status[len]=status[len-1];
         }
+
         names[0]=name;
         status[0]=statu;
         len++;
+
+    }
+
+    void remove_front(){
+        cout<<names[0]<<" please to the DR"<<endl;
+
+        for(int i=1;i<len;i++){
+            names[i-1]=names[i];
+            status[i-1]=status[i];
+        }
+
+        len--;
+    }
+
+    void print(){
+
+        for(int i=0;i<len;i++){
+            cout<<names<<" ";
+            
+            if(status[i])
+                cout<<"urgent"<<endl;
+            else 
+                cout<<"regular"<<endl;
+
+        }
     }
 
 
     
 };
+
+
+struct hospital_system
+{
+    hospital_queue queues[MAX_SPEC+1];
+
+    hospital_system(){
+        for(int i=0;i<MAX_SPEC;i++)
+            queues[i]=hospital_queue(i);
+    }
+};
+
 
 
 int main(){
